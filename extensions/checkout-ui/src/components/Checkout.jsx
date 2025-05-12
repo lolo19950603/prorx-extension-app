1c2b2ed0a45b3b6ad2eb26fd6cc6f5a890a84359
loren950603 <loren840603@gmail.com>
2025-05-12 11:45:37 -0400

    adding drop down list on checkout page

---
+++ extensions/checkout-ui/src/components/Checkout.jsx
@@ -0,0 +1,197 @@
+import {
+  Heading,
+  Select,
+  InlineStack,
+  TextField,
+  reactExtension,
+  Banner,
+  Checkbox,
+  BlockStack,
+  BlockSpacer,
+  Text,
+  useApi,
+  useApplyAttributeChange,
+  useInstructions,
+  useTranslate,
+} from "@shopify/ui-extensions-react/checkout";
+import { useState, useEffect } from "react";
+
+// import local dara
+import {
+  ProgramTypeList,  
+  IcsProgramList,
+  IcsSubProgramList,
+  ParamedProgramList
+} from "../data/BillingNumbers.jsx";
+
+// 1. Choose an extension target
+export default reactExtension("purchase.checkout.block.render", () => (
+  <Extension />
+));
+
+function Extension() {
+  const translate = useTranslate();
+  const { extension } = useApi();
+  const instructions = useInstructions();
+  const applyAttributeChange = useApplyAttributeChange();
+  const [selectedProgramOption, setSelectedProgramOption] = useState("");
+  const [subProgramName, setSubProgramName] = useState("");
+  const [selectedBilling, setSelectedBilling] = useState("");
+  const [billingNumber, setBillingNumber] = useState("");
+  const [programName, setProgramName] = useState("");
+
+    // This useEffect runs every time selectedProgram changes
+    useEffect(() => {
+      if (billingNumber !== "") {
+        applyAttributeChange({
+          key: "Program Info",
+          type: "updateAttribute",
+          value: programName + " " + billingNumber,
+        });
+      }
+      else if (billingNumber === "") {
+        applyAttributeChange({
+          key: "Program Info",
+          type: "updateAttribute",
+          value: "",
+        });
+      }
+    }, [billingNumber]);
+
+
+  const handleCheckboxChange = (option) => {
+    if (option === "Others") {
+      setSelectedProgramOption(option)
+      setProgramName("")
+      setBillingNumber("")
+      setSelectedBilling("")
+      setSubProgramName("")
+    }
+    else if (option === "Bayshore Consumable") {
+      setProgramName("Bayshore")
+      setBillingNumber("Consumable")
+      setSelectedProgramOption(option)
+    }
+    else {
+      setSelectedProgramOption(option)
+      setProgramName(option)
+      setBillingNumber("")
+      setSelectedBilling("")
+      setSubProgramName("")
+    }
+  };
+
+  const handleSubProgramChange = (option) => {
+    if (option !== subProgramName) {
+      setSubProgramName(option)
+      setBillingNumber("")
+    }
+    setSubProgramName(option)
+  };
+  // 3. Render a UI
+  return (
+    <BlockStack border={"dotted"} padding={"tight"}>
+      <Heading level={1}>Billing</Heading>
+      {/* display check boxes */}
+      <Select
+          label="Select Program"
+          options={Object.entries(ProgramTypeList).map(
+            ([key, value]) => ({
+              label: value,
+              value: value,
+            })
+          )}
+          value={selectedProgramOption}
+          onChange={handleCheckboxChange}
+          />
+      {/* display program detail section */}
+      {selectedProgramOption === "ICS" && (
+        <>
+          <Select
+          label="Select Program"
+          options={Object.entries(IcsProgramList).map(
+            ([key, value]) => ({
+              label: value,
+              value: value,
+            })
+          )}
+          value={subProgramName}
+          onChange={handleSubProgramChange}
+          />
+          {subProgramName && (
+            <Select
+            label="Select Program"
+            options={Object.entries(IcsSubProgramList[subProgramName]).map(
+              ([key, value]) => ({
+                label: value,
+                value: value,
+              })
+            )}
+            value={selectedBilling}
+            onChange={handleBillingChange}
+            />
+          )}
+        </>
+      )}
+      {selectedProgramOption === "Paramed" && (
+        <Select
+        label="Select Program"
+        options={Object.entries(ParamedProgramList).map(
+          ([key, value]) => ({
+            label: value,
+            value: value,
+          })
+        )}
+        value={selectedBilling}
+        onChange={handleBillingChange}
+        />
+      )}
+      {selectedProgramOption === "WSIB" && (
+        <>
+          <TextField
+          label={`${selectedProgramOption} Billing Number`}
+          name={`${selectedProgramOption} Billing Number`}
+          onChange={(value) => setBillingNumber(value)}
+          valuvalue={billingNumber}
+          />
+        </>
+      )}
+      {selectedProgramOption === "Bayshore Branch" && (
+        <>
+          <TextField
+          label={`${selectedProgramOption} Billing Number`}
+          name={`${selectedProgramOption} Billing Number`}
+          onChange={(value) => setBillingNumber(value)}
+          valuvalue={billingNumber}
+          />
+        </>
+      )}
+      {selectedProgramOption === "Others" && (
+        <>
+          <TextField
+          label="Program Name"
+          name="Program Name"
+          onChange={(value) => setProgramName(value)}
+          value={programName}
+          />
+          <TextField
+          label="Billing Number"
+          name="Billing Number"
+          onChange={(value) => setBillingNumber(value)}
+          valuvalue={billingNumber}
+          />
+        </>
+      )}
+    </BlockStack>
+  );
+
+  function handleBillingChange(value) {
+    setSelectedBilling(value);
+    if (subProgramName !== "") {
+      setBillingNumber(subProgramName+" "+value)
+    }
+    else {
+      setBillingNumber(value)
+    }
+  }
+}
---
+++ extensions/checkout-ui/src/data/BillingNumbers.jsx
@@ -0,0 +1,76 @@
+export const ProgramTypeList = {
+  1: "ICS",
+  2: "Paramed",
+  3: "WSIB",
+  4: "Bayshore Branch",
+  5: "Bayshore Consumable",
+  6: "Others",
+}
+
+export const IcsProgramList = {
+  1: "TCU",
+  2: "Community",
+  3: "H&K",
+  4: "NRIO",
+};
+
+export const IcsSubProgramList = {
+  TCU: {
+    1: "Abbeylawn - 1047090",
+    2: "Barrie Manor - 1046969",
+    3: "Carriage House - 1054343",
+    4: "Cedarbrook - 1063454",
+    5: "Greystone - 1059931",
+    6: "Sunrise Richmond Hill - 1052896",
+    7: "Sunrise Unionville - 1052891",
+    8: "The Windsor - 1054607",
+    9: "Traditions of Durham - 1047089",
+    10: "Waterside - 1047087",
+    11: "Weston Gardens - 1078995",
+    12: "Winchester Glen - 1118865",
+    13: "Sunrise of Aurora - 1128001",
+  },
+  Community: {
+    1: "Brant Home - 1127528",
+    2: "Brightshores Home - 1127524",
+    3: "Cambridge Home - 1127527",
+    4: "Collingwood@Home - 1118845",
+    5: "Community markham office - 1038195",
+    6: "Cornwall@Home - 1118847",
+    7: "Guelph Home - 1127529",
+    8: "Halton Hills@Home",
+    9: "Hamilton Home - 1127525",
+    10: "Headwater@Home - 1118849",
+    11: "Kingston@home - 1118851",
+    12: "Lakeridge@Home - 1118852",
+    13: "Markham@Homes-OVH - 1129189",
+    14: "Michael Garron Home - 1127308",
+    15: "Niagara Home - 1129188",
+    16: "NorthYork@Home - 1118853",
+    17: "NYCares - 1118854",
+    18: "RoyalVictoria@Home - 1118855",
+    19: "Scarborough@home - 1118856",
+    20: "Stevenson@Home - 1118857",
+    21: "St. Mary's @Home - 1118858",
+    22: "Sudbury H2H - 1129186",
+    23: "Timmins H2H - 1129187",
+    24: "Unity@Home - 1118859",
+    25: "Unity Enhanced@Home - 1135017",
+    26: "Waypoint Home - 1120279",
+    27: "William Osler Peritoneal Dialysis - 1118861",
+    28: "WilliamOsler@Home - 1118860",
+  },
+  "H&K": {
+    1: "Humber River - 1118862",
+    2: "Markham - 1118863",
+    3:"North York - 1118864",
+  },
+  NRIO: {
+    1: "NRIO-2125372"
+  },
+}
+
+export const ParamedProgramList = {
+  1: "Chattam-Kent - 1131530",
+  2: "Guelph OHT - 1131535"
+};
